c10b75e600cac37079fc7c8cf62148db4b1090df
Macoredroid <zhiyuanma0520@gmail.com>
2022-01-22 11:47:00 -0800

    update

--- src/main/java/com/justsayyes/record/Entity/Visitor.java
+++ src/main/java/com/justsayyes/record/Entity/Visitor.java
@@ -4,10 +4,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -23,6 +26,11 @@ public class Visitor {
 
     @Column
     String status;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "update_date")
+    private Date updateDate;
 
     @OneToMany(mappedBy = "visitor")
     private List<Record> records=new ArrayList<>();
